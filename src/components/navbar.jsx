d0f8aafc2e6ba471764e46517898695209057b5d
Aabhas <aabhassao0@gmail.com>
2021-09-27 20:42:19 +0530

    Feat: Project Structure setup
    Signed-off-by: Aabhas <aabhassao0@gmail.com>

---
+++ src/components/navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li><a href="@">Home</a></li>
+                <li><a href="@">Team</a></li>
+                <li><a href="@">About</a></li>
+                <li><a href="@">Projects</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
