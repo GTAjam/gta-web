52c453ceb22fabac2c87e129df44f140783a05d9
Aabhas <aabhassao0@gmail.com>
2021-12-14 15:47:04 +0530

    Feat: Initialize using nextjs
    Signed-off-by: Aabhas <aabhassao0@gmail.com>

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>GTA</title>
+      </Head>
+
+      <main>
+
+      </main>
+
+      <footer>
+
+      </footer>
+    </div>
+  )
+}
