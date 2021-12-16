1bbd228fe9369952feaa2213a840ba5d172b88bc
Aabhas <aabhassao0@gmail.com>
2021-12-16 13:41:00 +0530

    Feat: Add scoped scss example navbar component
    Signed-off-by: Aabhas <aabhassao0@gmail.com>

--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import styles from "./Navbar.module.scss";
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className={styles.st}>
       <ul>
-        <li>home</li>
+        <li className={styles.bt}>home</li>
         <li>about</li>
         <li>our team</li>
       </ul>
