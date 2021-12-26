1638f53b4c0413b568c4d1240154ff2b79643502
Mihir Bibhuty <77892621+MihirBibhuty@users.noreply.github.com>
2021-12-26 21:36:31 +0530

    Team Members

--- components/team/member.jsx
+++ components/team/member.jsx
@@ -1,10 +1,60 @@
 import React from "react";
+import Image from "next/image"
+// import Saswat from "./images/saswat.png";
 
-const Member = ({ name }) => {
+const Member = (props) => {
+
+  const cardStyle = {
+
+    width: "18rem",
+    // border: "2px solid #403f3f",
+    background: "linear-gradient(145deg, #23272b, #1e2024)",
+    // border: "2px solid #000000",
+    display: "inline-block",
+    boxShadow: "10px 10px 19px #1c1e22,-10px -10px 19px #262a2e, inset 21px 21px 19px #181a1d, inset -21px -21px 19px #202225, 1px 4px 2px -3px rgba(0, 0, 0, 0.7) inset, -1px -3px 3px -2px rgba(255, 255, 255, 0.2) inset",
+    "&:hover": {
+      cursor: "pointer",
+      border: "10px solid red"
+    }
+  }
+
   return (
-    <div>
-      <p>{name}</p>
+
+
+    <div style={{ minHeight: "100vh", backgroundColor: '#212428' }}>
+
+      {/* <img src={require('./images/saswat.png').default} alt="..." /> */}
+      {/* <Saswat src="/saswat.png" alt="me" /> */}
+
+      <Image src="/images/Image 1.jpeg"
+        width="100%"
+        height="100%"
+        objectFit="contain"
+        layout="responsive"
+        alt="img" />
+
+      <h2 className="text-center" style={{ fontSize: '3rem', fontFamily: 'Marcellus SC', marginBottom: '1rem', fontWeight: '600', lineHeight: 1.2, color: '#c4cfde' }}>Team Members</h2>
+      <div id="imgDiv" className="container" style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around' }}>
+
+        {props.data.map((images) => {
+          return (
+            <>
+
+              <div id='idNo' className="card my-3 mx-4" id="imgCard" style={cardStyle}>
+                <img src={images.img} className="card-img-top" alt="Image" height="224px" />
+                <div className="card-body">
+                  <h3 className="text-center text-light" style={{ fontFamily: 'Marcellus SC' }}>{images.text}</h3>
+                  <p className="card-text text-center" style={{ fontSize: '1.1rem', fontFamily: 'Roboto Condensed', color: '#b56d19', fontWeight: '600' }}>({images.designation})</p>
+                </div>
+              </div>
+
+            </>
+          )
+        })}
+
+      </div>
     </div>
+
   );
 };
 
