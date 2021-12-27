74775714e6fe490308aec41e73e2ff372cc0c129
Sakalabhaktula Hanisha <87903473+Sakalabhaktula-Hanisha@users.noreply.github.com>
2021-12-27 18:37:49 +0530

    Made changes in projectTrailer.jsx file
    Signed-off-by: Sakalabhaktula Hanisha <87903473+Sakalabhaktula-Hanisha@users.noreply.github.com>

--- components/projects/projectTrailer.jsx
+++ components/projects/projectTrailer.jsx
@@ -3,10 +3,23 @@ import Link from "next/link";
 
 const ProjectTrailer = () => {
   return (
-    <div>
-      <p>project 1</p>
-      <p>project 1</p>
-      <Link href="projects">view all</Link>
+    <div className="main">
+      <h1>Projects</h1>
+      <div className="container">
+        <div className="p1">
+          <h2>Project One</h2>
+          <img src="" alt="img" />
+          <p>This is an awesome project and it is very interesting too.</p>
+        </div>
+        <div className="p2">
+          <h2>Project Two</h2>
+          <img src="" alt="img" />
+          <p>This is an awesome project and it is very interesting too.</p>
+        </div>
+      </div>
+      <div className="btn">
+        <button className="butn"><a href="/projects" className="link">view all</a></button>
+      </div>
     </div>
   );
 };
