a2226ce30a3d040a7515e910edfc1a5439f1e644
Aabhas <aabhassao0@gmail.com>
2022-01-03 20:00:22 +0530

    Fix: video correct import
    Signed-off-by: Aabhas <aabhassao0@gmail.com>

--- components/hero/hero.jsx
+++ components/hero/hero.jsx
@@ -1,40 +1,36 @@
 import Link from "next/link";
-import bv from ".../public/bv.mp4";
-
 
 const Hero = () => {
+  return (
+    <div className="sec1">
+      <video
+        autoPlay
+        loop
+        muted
+        style={{
+          position: "absolute",
+          width: "100%",
+          zIndex: "-1",
+          top: 0,
+          left: 0,
+          height: "100vh",
+          objectFit: "cover",
+        }}
+      >
+        <source src="/bv.mp4" type="video/mp4" />
+      </video>
 
-    return (
-        <div className="sec1" >
-            <video autoPlay loop muted
-                style={{
-                    position: "absolute",
-                    width: "100%",
-                    zIndex: "-1",
-                    top: 0,
-                    left: 0,
-                    height: "100vh",
-                    objectFit: "cover",
-                }}>
-                <source src={bv} type="video/mp4" />
-            </video>
-
-            <div className="sec2"
-                style={{
-                    fontSize: "100px",
-                    marginTop: "300px"
-
-                }}>
-                WELCOME TO GTA
-
-
-            </div>
-
-        </div>
-
-    );
-
-
+      <div
+        className="sec2"
+        style={{
+          fontSize: "100px",
+          marginTop: "300px",
+        }}
+      >
+        WELCOME TO GTA
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
