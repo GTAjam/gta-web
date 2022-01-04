5fc7d0febdc0d6653ea9934166144ee2acbf1ba7
Mihir Bibhuty <77892621+MihirBibhuty@users.noreply.github.com>
2022-01-04 17:55:32 +0530

    Tabs added in members section

--- pages/team.jsx
+++ pages/team.jsx
@@ -1,5 +1,69 @@
 import React, { useState } from "react";
 import Image from "next/image";
+import { styled } from '@mui/system';
+import TabsUnstyled from '@mui/base/TabsUnstyled';
+import TabsListUnstyled from '@mui/base/TabsListUnstyled';
+import TabPanelUnstyled from '@mui/base/TabPanelUnstyled';
+import { buttonUnstyledClasses } from '@mui/base/ButtonUnstyled';
+import TabUnstyled, { tabUnstyledClasses } from '@mui/base/TabUnstyled';
+
+
+
+const Tab = styled(TabUnstyled)`
+  clip-path: polygon(11% 0, 98% 0, 89% 100%, 2% 100%);
+  font-family: Roboto Condensed;
+  color: #faf4f2; //inactive text color
+  cursor: pointer;
+  font-size: 0.9rem;
+  font-weight: bold;
+  background-color: transparent;
+  width: 100%;
+  padding: 8px 16px;
+  margin: 6px 6px;
+  border: none;
+  border-radius: 0px;
+  display: flex;
+  justify-content: center;
+
+  &:hover {
+    background-color: #ff8a65; //hover bg color
+  }
+
+  &.${buttonUnstyledClasses.focusVisible} {
+    color: #fff;
+    outline: none;
+    background-color: #ffab91;
+  }
+
+  &.${tabUnstyledClasses.selected} {
+    background-color: #faf4f2; //active bg
+    color: #ff3d00; //active text color
+  }
+
+  &.${buttonUnstyledClasses.disabled} {
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
+`;
+
+const TabPanel = styled(TabPanelUnstyled)`
+  width: 100%;
+  font-family: IBM Plex Sans, sans-serif;
+  font-size: 0.875rem;
+  color: #fff;
+`;
+
+const TabsList = styled(TabsListUnstyled)`
+  min-width: 320px;
+  background-color: #ff7043; //bg color
+  border-radius: 8px;
+  margin-bottom: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  align-content: space-between;
+`;
+
 
 const Team = () => {
 
@@ -250,192 +314,217 @@ const Team = () => {
   };
 
   return (
-    <div style={{ backgroundColor: "#212428", padding: "4rem 0rem" }}>
+    <div style={{ backgroundColor: "#212428", padding: "2rem 0rem 4rem 0rem" }}>
+
 
       <h2 className="text-center" style={{ fontSize: "3.5rem", fontFamily: "Marcellus SC", marginBottom: "2.5rem", fontWeight: "600", lineHeight: 1.2, color: "#c4cfde" }}>
         Team Members
       </h2>
 
 
-      {/* Part 1 */}
-      <h2 className="text-center" style={{ fontSize: "2.5rem", fontFamily: "Marcellus SC", fontWeight: "600", lineHeight: 1.2, color: "#c4cfde" }}>
-        Leaders
-      </h2>
-
-      <div id="imgDiv" className="container" style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-around", marginBottom: "2.5rem" }}>
-        {part1.map((images) => {
-          return (
-            <div id="idNo" className="box card my-3 mx-4" id="imgCard" style={cardStyle}>
-              <Image src={images.img} className="card-img-top" alt="Image" width="100%" height="80%" layout="responsive" objectFit="cover" />
-              <div className="card-body">
-                <h3 className="text-center text-light" style={{ fontFamily: "Marcellus SC" }}>
-                  {images.text}
-                </h3>
-                <p className="card-text text-center" style={{ fontSize: "1.1rem", fontFamily: "Roboto Condensed", color: "#b56d19", fontWeight: "600" }}>
-                  ({images.designation})
-                </p>
-              </div>
-            </div>
-          );
-        })}
-      </div>
 
 
-      {/* Part 2 */}
-      <h2 className="text-center" style={{ fontSize: "2.5rem", fontFamily: "Marcellus SC", fontWeight: "600", lineHeight: 1.2, color: "#c4cfde" }}>
-        Mentors
-      </h2>
+      <TabsUnstyled className="container" defaultValue={0}>
 
-      <div id="imgDiv" className="container" style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-around", marginBottom: "2.5rem" }}>
-        {part2.map((images) => {
-          return (
-            <>
-              <div id="idNo" className="box card my-3 mx-4" id="imgCard" style={cardStyle}>
-                <Image src={images.img} className="card-img-top" alt="Image" width="100%" height="80%" layout="responsive" objectFit="cover" />
-                <div className="card-body">
-                  <h3 className="text-center text-light" style={{ fontFamily: "Marcellus SC" }}>
-                    {images.text}
-                  </h3>
-                  <p className="card-text text-center" style={{ fontSize: "1.1rem", fontFamily: "Roboto Condensed", color: "#b56d19", fontWeight: "600" }}>
-                    ({images.designation})
-                  </p>
+        <TabsList style={{ borderRadius: "0", clipPath: "polygon(3.5% 0, 100% 0, 96.5% 100%, 0% 100%)", marginBottom: "2rem" }}>
+          <Tab>Executive Body</Tab>
+          <Tab>Co-ordinators</Tab>
+          <Tab>Teams</Tab>
+        </TabsList>
+
+        <TabPanel value={0}>
+
+          {/* Part 1 */}
+          <h2 className="text-center" style={{ fontSize: "2.5rem", fontFamily: "Marcellus SC", fontWeight: "600", lineHeight: 1.2, color: "#c4cfde" }}>
+            Leaders
+          </h2>
+
+          <div id="imgDiv" className="container" style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-around", marginBottom: "2.5rem" }}>
+            {part1.map((images) => {
+              return (
+                <div id="idNo" className="box card my-3 mx-4" id="imgCard" style={cardStyle}>
+                  <Image src={images.img} className="card-img-top" alt="Image" width="100%" height="80%" layout="responsive" objectFit="cover" />
+                  <div className="card-body">
+                    <h3 className="text-center text-light" style={{ fontFamily: "Marcellus SC" }}>
+                      {images.text}
+                    </h3>
+                    <p className="card-text text-center" style={{ fontSize: "1.1rem", fontFamily: "Roboto Condensed", color: "#b56d19", fontWeight: "600" }}>
+                      ({images.designation})
+                    </p>
+                  </div>
                 </div>
-              </div>
-            </>
-          );
-        })}
-      </div>
+              );
+            })}
+          </div>
+
+          {/* Part 2 */}
+          <h2 className="text-center" style={{ fontSize: "2.5rem", fontFamily: "Marcellus SC", fontWeight: "600", lineHeight: 1.2, color: "#c4cfde" }}>
+            Mentors
+          </h2>
+
+          <div id="imgDiv" className="container" style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-around", marginBottom: "2.5rem" }}>
+            {part2.map((images) => {
+              return (
+                <>
+                  <div id="idNo" className="box card my-3 mx-4" id="imgCard" style={cardStyle}>
+                    <Image src={images.img} className="card-img-top" alt="Image" width="100%" height="80%" layout="responsive" objectFit="cover" />
+                    <div className="card-body">
+                      <h3 className="text-center text-light" style={{ fontFamily: "Marcellus SC" }}>
+                        {images.text}
+                      </h3>
+                      <p className="card-text text-center" style={{ fontSize: "1.1rem", fontFamily: "Roboto Condensed", color: "#b56d19", fontWeight: "600" }}>
+                        ({images.designation})
+                      </p>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
+          </div>
+
+        </TabPanel>
+
+        <TabPanel value={1}>
+
+          {/* Part 3 */}
+          <h2 className="text-center" style={{ fontSize: "2.5rem", fontFamily: "Marcellus SC", fontWeight: "600", lineHeight: 1.2, color: "#c4cfde" }}>
+            Co-ordinators
+          </h2>
+
+          <div id="imgDiv" className="container" style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-around", marginBottom: "2.5rem" }}>
+            {part3.map((images) => {
+              return (
+                <>
+                  <div id="idNo" className="box card my-3 mx-4" id="imgCard" style={cardStyle}>
+                    <Image src={images.img} className="card-img-top" alt="Image" width="100%" height="80%" layout="responsive" objectFit="cover" />
+                    <div className="card-body">
+                      <h3 className="text-center text-light" style={{ fontFamily: "Marcellus SC" }}>
+                        {images.text}
+                      </h3>
+                      <p className="card-text text-center" style={{ fontSize: "1.1rem", fontFamily: "Roboto Condensed", color: "#b56d19", fontWeight: "600" }}>
+                        ({images.designation})
+                      </p>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
+          </div>
+
+        </TabPanel>
 
 
-      {/* Part 3 */}
-      <h2 className="text-center" style={{ fontSize: "2.5rem", fontFamily: "Marcellus SC", fontWeight: "600", lineHeight: 1.2, color: "#c4cfde" }}>
-        Co-ordinators
-      </h2>
 
-      <div id="imgDiv" className="container" style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-around", marginBottom: "2.5rem" }}>
-        {part3.map((images) => {
-          return (
-            <>
-              <div id="idNo" className="box card my-3 mx-4" id="imgCard" style={cardStyle}>
-                <Image src={images.img} className="card-img-top" alt="Image" width="100%" height="80%" layout="responsive" objectFit="cover" />
-                <div className="card-body">
-                  <h3 className="text-center text-light" style={{ fontFamily: "Marcellus SC" }}>
-                    {images.text}
-                  </h3>
-                  <p className="card-text text-center" style={{ fontSize: "1.1rem", fontFamily: "Roboto Condensed", color: "#b56d19", fontWeight: "600" }}>
-                    ({images.designation})
-                  </p>
-                </div>
-              </div>
-            </>
-          );
-        })}
-      </div>
+        <TabPanel value={2}>
 
+          {/* Part 4 */}
+          <h2 className="text-center" style={{ fontSize: "2.5rem", fontFamily: "Marcellus SC", fontWeight: "600", lineHeight: 1.2, color: "#c4cfde" }}>
+            Code Team
+          </h2>
 
-      {/* Part 4 */}
-      <h2 className="text-center" style={{ fontSize: "2.5rem", fontFamily: "Marcellus SC", fontWeight: "600", lineHeight: 1.2, color: "#c4cfde" }}>
-        Code Team
-      </h2>
-
-      <div id="imgDiv" className="container" style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-around", marginBottom: "2.5rem" }}>
-        {part4.map((images) => {
-          return (
-            <>
-              <div id="idNo" className="box card my-3 mx-4" id="imgCard" style={cardStyle}>
-                <Image src={images.img} className="card-img-top" alt="Image" width="100%" height="80%" layout="responsive" objectFit="cover" />
-                <div className="card-body">
-                  <h3 className="text-center text-light" style={{ fontFamily: "Marcellus SC" }}>
-                    {images.text}
-                  </h3>
-                  <p className="card-text text-center" style={{ fontSize: "1.1rem", fontFamily: "Roboto Condensed", color: "#b56d19", fontWeight: "600" }}>
-                    ({images.designation})
-                  </p>
-                </div>
-              </div>
-            </>
-          );
-        })}
-      </div>
+          <div id="imgDiv" className="container" style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-around", marginBottom: "2.5rem" }}>
+            {part4.map((images) => {
+              return (
+                <>
+                  <div id="idNo" className="box card my-3 mx-4" id="imgCard" style={cardStyle}>
+                    <Image src={images.img} className="card-img-top" alt="Image" width="100%" height="80%" layout="responsive" objectFit="cover" />
+                    <div className="card-body">
+                      <h3 className="text-center text-light" style={{ fontFamily: "Marcellus SC" }}>
+                        {images.text}
+                      </h3>
+                      <p className="card-text text-center" style={{ fontSize: "1.1rem", fontFamily: "Roboto Condensed", color: "#b56d19", fontWeight: "600" }}>
+                        ({images.designation})
+                      </p>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
+          </div>
 
 
-      {/* Part 5 */}
-      <h2 className="text-center" style={{ fontSize: "2.5rem", fontFamily: "Marcellus SC", fontWeight: "600", lineHeight: 1.2, color: "#c4cfde" }}>
-        Web Dev Team
-      </h2>
+          {/* Part 5 */}
+          <h2 className="text-center" style={{ fontSize: "2.5rem", fontFamily: "Marcellus SC", fontWeight: "600", lineHeight: 1.2, color: "#c4cfde" }}>
+            Web Dev Team
+          </h2>
 
-      <div id="imgDiv" className="container" style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-around", marginBottom: "2.5rem" }}>
-        {part5.map((images) => {
-          return (
-            <>
-              <div id="idNo" className="box card my-3 mx-4" id="imgCard" style={cardStyle}>
-                <Image src={images.img} className="card-img-top" alt="Image" width="100%" height="80%" layout="responsive" objectFit="cover" />
-                <div className="card-body">
-                  <h3 className="text-center text-light" style={{ fontFamily: "Marcellus SC" }}>
-                    {images.text}
-                  </h3>
-                  <p className="card-text text-center" style={{ fontSize: "1.1rem", fontFamily: "Roboto Condensed", color: "#b56d19", fontWeight: "600" }}>
-                    ({images.designation})
-                  </p>
-                </div>
-              </div>
-            </>
-          );
-        })}
-      </div>
-
-
-      {/* Part 6 */}
-      <h2 className="text-center" style={{ fontSize: "2.5rem", fontFamily: "Marcellus SC", fontWeight: "600", lineHeight: 1.2, color: "#c4cfde" }}>
-        Design Team
-      </h2>
-
-      <div id="imgDiv" className="container" style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-around", marginBottom: "2.5rem" }}>
-        {part6.map((images) => {
-          return (
-            <>
-              <div id="idNo" className="box card my-3 mx-4" id="imgCard" style={cardStyle}>
-                <Image src={images.img} className="card-img-top" alt="Image" width="100%" height="80%" layout="responsive" objectFit="cover" />
-                <div className="card-body">
-                  <h3 className="text-center text-light" style={{ fontFamily: "Marcellus SC" }}>
-                    {images.text}
-                  </h3>
-                  <p className="card-text text-center" style={{ fontSize: "1.1rem", fontFamily: "Roboto Condensed", color: "#b56d19", fontWeight: "600" }}>
-                    ({images.designation})
-                  </p>
-                </div>
-              </div>
-            </>
-          );
-        })}
-      </div>
+          <div id="imgDiv" className="container" style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-around", marginBottom: "2.5rem" }}>
+            {part5.map((images) => {
+              return (
+                <>
+                  <div id="idNo" className="box card my-3 mx-4" id="imgCard" style={cardStyle}>
+                    <Image src={images.img} className="card-img-top" alt="Image" width="100%" height="80%" layout="responsive" objectFit="cover" />
+                    <div className="card-body">
+                      <h3 className="text-center text-light" style={{ fontFamily: "Marcellus SC" }}>
+                        {images.text}
+                      </h3>
+                      <p className="card-text text-center" style={{ fontSize: "1.1rem", fontFamily: "Roboto Condensed", color: "#b56d19", fontWeight: "600" }}>
+                        ({images.designation})
+                      </p>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
+          </div>
 
 
-      {/* Part 7 */}
-      <h2 className="text-center" style={{ fontSize: "2.5rem", fontFamily: "Marcellus SC", fontWeight: "600", lineHeight: 1.2, color: "#c4cfde" }}>
-        Content Team
-      </h2>
+          {/* Part 6 */}
+          <h2 className="text-center" style={{ fontSize: "2.5rem", fontFamily: "Marcellus SC", fontWeight: "600", lineHeight: 1.2, color: "#c4cfde" }}>
+            Design Team
+          </h2>
 
-      <div id="imgDiv" className="container" style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-around" }}>
-        {part7.map((images) => {
-          return (
-            <>
-              <div id="idNo" className="box card my-3 mx-4" id="imgCard" style={cardStyle}>
-                <Image src={images.img} className="card-img-top" alt="Image" width="100%" height="80%" layout="responsive" objectFit="cover" />
-                <div className="card-body">
-                  <h3 className="text-center text-light" style={{ fontFamily: "Marcellus SC" }}>
-                    {images.text}
-                  </h3>
-                  <p className="card-text text-center" style={{ fontSize: "1.1rem", fontFamily: "Roboto Condensed", color: "#b56d19", fontWeight: "600" }}>
-                    ({images.designation})
-                  </p>
-                </div>
-              </div>
-            </>
-          );
-        })}
-      </div>
+          <div id="imgDiv" className="container" style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-around", marginBottom: "2.5rem" }}>
+            {part6.map((images) => {
+              return (
+                <>
+                  <div id="idNo" className="box card my-3 mx-4" id="imgCard" style={cardStyle}>
+                    <Image src={images.img} className="card-img-top" alt="Image" width="100%" height="80%" layout="responsive" objectFit="cover" />
+                    <div className="card-body">
+                      <h3 className="text-center text-light" style={{ fontFamily: "Marcellus SC" }}>
+                        {images.text}
+                      </h3>
+                      <p className="card-text text-center" style={{ fontSize: "1.1rem", fontFamily: "Roboto Condensed", color: "#b56d19", fontWeight: "600" }}>
+                        ({images.designation})
+                      </p>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
+          </div>
 
+
+          {/* Part 7 */}
+          <h2 className="text-center" style={{ fontSize: "2.5rem", fontFamily: "Marcellus SC", fontWeight: "600", lineHeight: 1.2, color: "#c4cfde" }}>
+            Content Team
+          </h2>
+
+          <div id="imgDiv" className="container" style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-around" }}>
+            {part7.map((images) => {
+              return (
+                <>
+                  <div id="idNo" className="box card my-3 mx-4" id="imgCard" style={cardStyle}>
+                    <Image src={images.img} className="card-img-top" alt="Image" width="100%" height="80%" layout="responsive" objectFit="cover" />
+                    <div className="card-body">
+                      <h3 className="text-center text-light" style={{ fontFamily: "Marcellus SC" }}>
+                        {images.text}
+                      </h3>
+                      <p className="card-text text-center" style={{ fontSize: "1.1rem", fontFamily: "Roboto Condensed", color: "#b56d19", fontWeight: "600" }}>
+                        ({images.designation})
+                      </p>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
+          </div>
+
+
+        </TabPanel>
+
+
+      </TabsUnstyled>
 
 
     </div>
