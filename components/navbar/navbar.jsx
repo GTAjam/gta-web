7bb68b6d2759094491d7b8cb9c1c42ac2d44e605
anubhab555 <anubhabmaharana555@gmail.com>
2022-01-11 12:17:31 +0530

    navbar

--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -3,12 +3,28 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li>home</li>
-        <li>about</li>
-        <li><Link href="/team">our team</Link></li>
-      </ul>
+    /*Navbar*/
+    <nav className="navbar navbar-expand-lg navbar-dark">
+      <a className="navbar-brand" href="">GTA</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <a className="nav-link" href="">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="">About</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/team">Our Team</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/projects">Projects</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
